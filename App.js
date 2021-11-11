d85e7388d4e886c0b6d4b0ca45650bc17e112185
WebDev-Chad <chadwin.allotey@gmail.com>
2021-11-10 22:50:46 -0500

    update

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Entypo } from "@expo/vector-icons";
+import { Button } from "native-base";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+let itemArray = new Array(9).fill("empty");
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isCircle: false,
+      winMessage: "",
+    };
+  }
+
+  drawItem = (itemNumber) => {
+    if (itemArray[itemNumber] === "empty") {
+      itemArray[itemNumber] = this.state.isCircle;
+      this.setState({ isCircle: !itemArray[itemNumber] }, () => {});
+    }
+  };
+
+  chooseItemIcon = (itemNumber) => {
+    if (itemArray[itemNumber] !== "empty") {
+      return itemArray[itemNumber] ? "circle" : "cross";
+    }
+    return "pencil";
+  };
+
+  chooseItemColor = (itemNumber) => {
+    if (itemArray[itemNumber] !== "empty") {
+      return itemArray[itemNumber] ? "green" : "red";
+    }
+    return "black";
+  };
+
+  resetGame = () => {
+    itemArray.fill("empty");
+    this.setState({ winMessage: "" });
+    //force update to component
+    this.forceUpdate();
+  };
+
+  winGame = () => {};
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your555 app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
