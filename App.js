415c695e638d87f3a8fcd0de342f293c603fbba8
WebDev-Chad <chadwin.allotey@gmail.com>
2021-11-11 21:12:53 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Entypo } from "@expo/vector-icons";
-import { Button } from "native-base";
+import { NativeBaseProvider, Button } from "native-base";
 
 let itemArray = new Array(9).fill("empty");
 class App extends Component {
@@ -50,7 +50,7 @@ class App extends Component {
       itemArray[1] == itemArray[2]
     ) {
       this.setState({
-        winMessage: (itemArray[0] ? "Circle" : "Cross").concat(" win")
+        winMessage: (itemArray[0] ? "Circle" : "Cross").concat(" win"),
       });
     } else if (
       itemArray[3] !== "empty" &&
@@ -58,7 +58,7 @@ class App extends Component {
       itemArray[4] == itemArray[5]
     ) {
       this.setState({
-        winMessage: (itemArray[0] ? "Circle" : "Cross").concat(" win")
+        winMessage: (itemArray[0] ? "Circle" : "Cross").concat(" win"),
       });
     } else if (
       itemArray[6] !== "empty" &&
@@ -66,7 +66,7 @@ class App extends Component {
       itemArray[7] == itemArray[8]
     ) {
       this.setState({
-        winMessage: (itemArray[6] ? "Circle" : "Cross").concat(" win")
+        winMessage: (itemArray[6] ? "Circle" : "Cross").concat(" win"),
       });
     } else if (
       itemArray[0] !== "empty" &&
@@ -74,7 +74,7 @@ class App extends Component {
       itemArray[3] == itemArray[6]
     ) {
       this.setState({
-        winMessage: (itemArray[0] ? "Circle" : "Cross").concat(" win")
+        winMessage: (itemArray[0] ? "Circle" : "Cross").concat(" win"),
       });
     } else if (
       itemArray[1] !== "empty" &&
@@ -82,7 +82,7 @@ class App extends Component {
       itemArray[4] == itemArray[7]
     ) {
       this.setState({
-        winMessage: (itemArray[1] ? "Circle" : "Cross").concat(" win")
+        winMessage: (itemArray[1] ? "Circle" : "Cross").concat(" win"),
       });
     } else if (
       itemArray[2] !== "empty" &&
@@ -90,7 +90,7 @@ class App extends Component {
       itemArray[5] == itemArray[8]
     ) {
       this.setState({
-        winMessage: (itemArray[2] ? "Circle" : "Cross").concat(" win")
+        winMessage: (itemArray[2] ? "Circle" : "Cross").concat(" win"),
       });
     } else if (
       itemArray[0] !== "empty" &&
@@ -98,7 +98,7 @@ class App extends Component {
       itemArray[4] == itemArray[8]
     ) {
       this.setState({
-        winMessage: (itemArray[0] ? "Circle" : "Cross").concat(" win")
+        winMessage: (itemArray[0] ? "Circle" : "Cross").concat(" win"),
       });
     } else if (
       itemArray[2] !== "empty" &&
@@ -106,18 +106,112 @@ class App extends Component {
       itemArray[4] == itemArray[6]
     ) {
       this.setState({
-        winMessage: (itemArray[2] ? "Circle" : "Cross").concat(" win")
+        winMessage: (itemArray[2] ? "Circle" : "Cross").concat(" win"),
       });
     }
   };
 
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NativeBaseProvider>
+        <View style={styles.container}>
+          <Text style={styles.gametitle}>---- Tic Tac Toe----</Text>
+          <View style={styles.grid}>
+            <View style={styles.row}>
+              <View style={styles.item}>
+                <TouchableOpacity onPress={() => this.drawItem(0)}>
+                  <Entypo
+                    name={this.chooseItemIcon(0)}
+                    size={50}
+                    color={this.chooseItemColor(0)}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.item}>
+                <TouchableOpacity onPress={() => this.drawItem(1)}>
+                  <Entypo
+                    name={this.chooseItemIcon(1)}
+                    size={50}
+                    color={this.chooseItemColor(1)}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.item}>
+                <TouchableOpacity onPress={() => this.drawItem(2)}>
+                  <Entypo
+                    name={this.chooseItemIcon(2)}
+                    size={50}
+                    color={this.chooseItemColor(2)}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.row}>
+              <View style={styles.item}>
+                <TouchableOpacity onPress={() => this.drawItem(3)}>
+                  <Entypo
+                    name={this.chooseItemIcon(3)}
+                    size={50}
+                    color={this.chooseItemColor(3)}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.item}>
+                <TouchableOpacity onPress={() => this.drawItem(4)}>
+                  <Entypo
+                    name={this.chooseItemIcon(4)}
+                    size={50}
+                    color={this.chooseItemColor(4)}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.item}>
+                <TouchableOpacity onPress={() => this.drawItem(5)}>
+                  <Entypo
+                    name={this.chooseItemIcon(5)}
+                    size={50}
+                    color={this.chooseItemColor(5)}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.row}>
+              <View style={styles.item}>
+                <TouchableOpacity onPress={() => this.drawItem(6)}>
+                  <Entypo
+                    name={this.chooseItemIcon(6)}
+                    size={50}
+                    color={this.chooseItemColor(6)}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.item}>
+                <TouchableOpacity onPress={() => this.drawItem(7)}>
+                  <Entypo
+                    name={this.chooseItemIcon(7)}
+                    size={50}
+                    color={this.chooseItemColor(7)}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.item}>
+                <TouchableOpacity onPress={() => this.drawItem(8)}>
+                  <Entypo
+                    name={this.chooseItemIcon(8)}
+                    size={50}
+                    color={this.chooseItemColor(8)}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+          <StatusBar style="auto" />
+          <Text style={styles.winMessage}>{this.state.winMessage}</Text>
+          <Button style={styles.button} onPress={this.resetGame}>
+            <Text style={styles.btntext}>Reset Game</Text>
+          </Button>
+        </View>
+      </NativeBaseProvider>
     );
   }
 }
@@ -128,6 +222,32 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  gametitle: {
+    fontSize: 25,
+    marginBottom: 20,
+  },
+  grid: {},
+  row: {
+    flexDirection: "row",
+  },
+  item: {
+    borderWidth: 2,
+    borderColor: "#000",
+    padding: 30,
+  },
+  winMessage: {
+    padding: 20,
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  button: {
+    margin: 20,
+    padding: 10,
+  },
+  btntext: {
+    color: "#FFF",
+    fontWeight: "bold",
   },
 });
 
